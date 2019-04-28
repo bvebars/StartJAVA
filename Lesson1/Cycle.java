98885fe1df813301cb6c3eb0f7b53282f2f6eaed
bvebars <37873824+bvebars@users.noreply.github.com>
2019-04-28 23:16:07 +0700

    Update Cycle.java

--- Lesson1/Cycle.java
+++ Lesson1/Cycle.java
@@ -2,26 +2,26 @@ public class Cycle {
     public static void main(String[] args) {
 
         //for все числа от [0, 20]
-        for (int i_1 = 0; i_1 <= 20; i_1++) {
-            System.out.println(i_1);
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
         }
         //while все числа от [6, -6] (шаг итерации равен 2)
-        int i_2 = 6;
-        while ( i_2 >= -6 ) {
-            System.out.println(i_2);
-            i_2 = i_2 - 2;
+        int j = 6;
+        while ( j >= -6 ) {
+            System.out.println(j);
+            ij = j - 2;
         }
 
         //do-while сумму всех нечетных чисел от [10, 20]
-        int i_3 = 10;
+        int v = 10;
         int sum = 0;
 
         do {
-            if (i_3% 2 == 0 ) {
-                sum += i_3;
+            if (v% 2 == 0 ) {
+                sum += v;
             }
-            i_3++;
-        } while ( i_3 <= 20);
+            v++;
+        } while ( v <= 20);
         System.out.println(sum);
     }
 }
