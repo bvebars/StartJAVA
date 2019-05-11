560992610e695f13195a1bc6a2656041ed3bc773
topjava <bvebars@gmail.com>
2019-05-11 19:56:35 +0700

    Добавлен калькулятор

---
+++ Lesson1/calculator.java
@@ -0,0 +1,36 @@
+
+public class Calculator {
+    public static void main(String[] args) {
+
+
+        int a = 50;  // Число 1
+        int b = 4;  // Число 2
+        String operator = "%"; //Оператор
+
+        if (a > 0 || b > 0) {
+            if (operator == "+") {
+                System.out.println(a + b);
+
+            } else if (operator == "-") {
+                System.out.println(a - b);
+
+            } else if (operator == "*") {
+                System.out.println(a * b);
+
+            } else if (operator == "/") {
+                System.out.println(a / b);
+
+            } else if (operator == "^") {
+                for (int i = 0; i < b - 1; i++) {
+                    a = a * b;
+                }
+                System.out.println(a);
+            } else if (operator == "%") {
+
+                int res = b * a / 100;
+                System.out.println(res + "%");
+
+            }
+        }
+    }
+}
