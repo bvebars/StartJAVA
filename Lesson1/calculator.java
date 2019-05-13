e13ccc306f58229439b7001fc555fbb5bc567f62
topjava <bvebars@gmail.com>
2019-05-13 21:51:20 +0700

    Переименование файлов

--- Lesson1/calculator.java
+++ Lesson1/calculator.java
@@ -1,35 +1,29 @@
-
 public class Calculator {
     public static void main(String[] args) {
-
-
-        int a = 50;  // Число 1
+        int a = 4;  // Число 1
         int b = 4;  // Число 2
-        String operator = "%"; //Оператор
+        String operator = "^"; //Оператор
 
         if (a > 0 || b > 0) {
             if (operator == "+") {
                 System.out.println(a + b);
-
             } else if (operator == "-") {
                 System.out.println(a - b);
-
             } else if (operator == "*") {
                 System.out.println(a * b);
-
             } else if (operator == "/") {
                 System.out.println(a / b);
-
             } else if (operator == "^") {
-                for (int i = 0; i < b - 1; i++) {
-                    a = a * b;
+                if (a > 0 && b > 0) {
+                    for (int i = 0; i < b - 1; i++) {
+                        a *= b;
+                    }
+                } else {
+                    a = 0;
                 }
                 System.out.println(a);
-             } else if (operator == "%") {
-
-                int res = a % b;
-                System.out.println(res + "%");
-
+            } else if (operator == "%") {
+                System.out.println(a % b + "%");
             }
         }
     }
