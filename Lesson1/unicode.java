f03f9ef5b64ad5338c4022b13ce1cc8475a226de
topjava <bvebars@gmail.com>
2019-05-13 21:49:46 +0700

    Переименование файлов

--- Lesson1/unicode.java
+++ Lesson1/unicode.java
@@ -1,21 +1,8 @@
-public class unicode {
+public class Unicode {
     public static void main(String[] args) {
-        String s = "9398";
-        char s1 = s.toCharArray()[0];
-
-        System.out.print(s1);
-
-        int a = 9398;
-        int b = 10178;
-
-        for (int i = a; i < b; i++) {
-
-            String str = Integer.toString(i);
-            char simvol = (char)Integer.parseInt(str);
+        for (int i = 9398; i < 10178; i++) {
+            char simvol = (char) i;
             System.out.println(simvol);
-
         }
-
     }
-
 }
