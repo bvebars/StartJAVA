b315aab243bba5ffa3989449c7769ca9827f0b54
topjava <bvebars@gmail.com>
2019-05-14 20:08:25 +0700

    Доработка

--- Lesson1/unicode.java
+++ Lesson1/unicode.java
@@ -1,8 +1,7 @@
 public class Unicode {
     public static void main(String[] args) {
         for (int i = 9398; i < 10178; i++) {
-            char simvol = (char) i;
-            System.out.println(simvol);
+            System.out.println((char) i);
         }
     }
 }
