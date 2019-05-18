544827dd9dfa4b6885bfd4a69ac52bd132facc09
topjava <bvebars@gmail.com>
2019-05-18 20:53:49 +0700

    Создание нового раздела

---
+++ Lesson2/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+    public static void main(String[] args) {
+
+        String name = "Vyacheslav";
+        String sex = "men";
+        int growth = 170;
+        int weight = 70;
+        int age = 28;
+
+        boolean go() {
+
+        }
+
+        boolean sit() {
+
+        }
+
+        boolean run() {
+
+        }
+
+        boolean say() {
+
+        }
+
+        boolean teachJava() {
+
+        }
+    }
+}
