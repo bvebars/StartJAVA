3724c2cb0371979fa45284126e2ad5d1feab8032
topjava <bvebars@gmail.com>
2019-05-20 21:05:09 +0700

    Доработка

--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -4,33 +4,53 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int a = scan.nextInt();
-        String operator = scan.next();
-        int b = scan.nextInt();
+        boolean game = true;
 
-        switch (operator) {
-            case "-":
-                System.out.println(a - b);
-                break;
-            case "+":
-                System.out.println(a + b);
-                break;
-            case "*":
-                System.out.println(a * b);
-                break;
-            case "/":
-                System.out.println(a / b);
-                break;
-            case "^":
-                int res = 1;
-                for (int i = 1; i <= b; i++) {
-                    res *= a;
-                }
-                System.out.println(res);
-                break;
-            case "%":
-                System.out.println(a % b + "%");
-                break;
+        while (game) {
+            System.out.println("Введите числа:");
+
+            int a = scan.nextInt();
+            String operator = scan.next();
+            int b = scan.nextInt();
+
+            switch (operator) {
+                case "-":
+                    System.out.println(a - b);
+                    break;
+                case "+":
+                    System.out.println(a + b);
+                    break;
+                case "*":
+                    System.out.println(a * b);
+                    break;
+                case "/":
+                    System.out.println(a / b);
+                    break;
+                case "^":
+                    int res = 1;
+                    for (int i = 1; i <= b; i++) {
+                        res *= a;
+                    }
+                    System.out.println("результат: "+ res);
+                    break;
+                case "%":
+                    System.out.println(a % b + "%");
+                    break;
+            }
+            System.out.println("Хотите продолжить? [да/нет]: ");
+
+            String continueGame = scan.next();
+
+            switch (continueGame) {
+                case "да":
+                    game = true;
+                    break;
+                case "нет":
+                    game = false;
+                    break;
+                default:
+                    System.out.println("Введите корректную строку");
+            }
         }
     }
 }
