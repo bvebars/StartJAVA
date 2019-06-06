53673ca5705ef129f18b8ca0f8e932573378c6c4
topjava <bvebars@gmail.com>
2019-06-06 21:41:23 +0700

    Доработка

---
+++ src/com/startjava/lesson_2/game/GuessNumberTest.java
@@ -0,0 +1,27 @@
+package com.startjava.lesson_2.game;
+
+import java.util.Scanner;
+
+public class GuessNumberTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите имя первого игрока: ");
+        Player player1 = new Player(scanner.nextLine());
+        System.out.print("Введите имя второго игрока: ");
+        Player player2 = new Player(scanner.nextLine());
+
+        GuessNumber guessNumber = new GuessNumber(player1, player2);
+        String answer;
+
+        do {
+            guessNumber.guessNumber();
+
+            do {
+                System.out.println("Хотите продолжить? [да/нет]:");
+                answer = scanner.nextLine();
+            } while (!answer.equals("да") && !answer.equals("нет"));
+        } while (answer.equals("да"));
+        guessNumber.incrementTotal();
+    }
+}
